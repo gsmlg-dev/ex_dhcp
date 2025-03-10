eb4f1f3458639b2ca5b2edf66287f5b370bbef59
Jonathan Gao <gsmlg.com@gmail.com>
2025-03-10 20:46:36 +0800

    chore: Fix publish.

--- mix.exs
+++ mix.exs
@@ -1,28 +1,87 @@
 defmodule DHCP.MixProject do
-  use Mix.Project
+  @source_url "https://github.com/gsmlg-dev/ex_dhcp.git"
+  @version "0.0.0"
 
   def project do
     [
       app: :dhcp_ex,
-      version: "0.1.0",
-      elixir: "~> 1.17",
+      version: @version,
+      elixir: "~> 1.13",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      name: "DHCP",
+      description: "DHCP resource definition in Pure Elixir",
+      source_url: @source_url,
+      dialyzer: dialyzer(),
+      aliases: aliases(),
+      package: package(),
+      deps: deps(),
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp deps() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.25", runtime: false},
+      {:machete, ">= 0.0.0", only: [:dev, :test]},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+      plt_add_deps: :apps_direct,
+      plt_add_apps: [:public_key],
+      flags: [
+        "-Werror_handling",
+        "-Wextra_return",
+        "-Wmissing_return",
+        "-Wunknown",
+        "-Wunmatched_returns",
+        "-Wunderspecs"
+      ]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Jonathan Gao"],
+      licenses: ["MIT"],
+      files: ~w(lib LICENSE mix.exs README.md),
+      links: %{
+        Github: @source_url,
+        Changelog: "https://hexdocs.pm/dhcp_ex/changelog.html"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_url: @source_url,
+      source_ref: "v#{@version}"
+    ]
+  end
+
+  defp aliases do
+    [
+      publish: [
+        "format",
+        "hex.publish --yes"
+      ]
     ]
   end
 end
